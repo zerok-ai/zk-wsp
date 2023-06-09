a9432087cb14798e5298bbcf4dcda4551b826983
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-06-09 16:28:19 +0530

    Fixed the issue of reading from config yaml file

--- utils/utils.go
+++ utils/utils.go
@@ -3,37 +3,30 @@ package utils
 import (
 	"flag"
 	"fmt"
-	"github.com/ilyakaznacheev/cleanenv"
+	"gopkg.in/yaml.v3"
 	"os"
 )
 
-type Args struct {
-	ConfigPath string
-}
+func ProcessArgs(config interface{}) error {
+	filePath := flag.String("c", "", "config file path")
+	flag.Parse()
 
-func ProcessArgs(cfg interface{}) error {
-	var a Args
-
-	flagSet := flag.NewFlagSet("server", 1)
-	flagSet.StringVar(&a.ConfigPath, "c", "config.yaml", "Path to configuration file")
-
-	fu := flagSet.Usage
-	flagSet.Usage = func() {
-		fu()
-		envHelp, _ := cleanenv.GetDescription(cfg, nil)
-		if _, err := fmt.Fprintln(flagSet.Output()); err != nil {
-			return
-		}
-
-		_, err := fmt.Fprintln(flagSet.Output(), envHelp)
-		if err != nil {
-			return
-		}
+	if *filePath == "" {
+		fmt.Println("Please provide a file path using the -c option.")
+		return fmt.Errorf("file path provided is empty")
 	}
 
-	if err := flagSet.Parse(os.Args[1:]); err != nil {
+	fileContents, err := os.ReadFile(*filePath)
+	if err != nil {
+		fmt.Printf("Failed to read file: %s\n", err)
 		return err
 	}
 
-	return cleanenv.ReadConfig(a.ConfigPath, &cfg)
+	err = yaml.Unmarshal(fileContents, config)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal yaml: %s\n", err)
+		return err
+	}
+
+	return nil
 }
