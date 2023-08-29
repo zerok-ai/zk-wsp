3767ee8a0df8d5e162796f6a1e76b1b97c3d1f6e
avingoyal <avingoyal01+github@gmail.com>
2023-08-29 22:44:52 +0530

    using common git actions for build and push

--- helm-charts/rename-docker-repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DOCKER_REPO=$1
-
-echo "THIS_DIR=$THIS_DIR"
-echo "DOCKER_REPO=$DOCKER_REPO"
-
-# Set the image tag based on the chart version
-perl -pi -e "s#dockerBase: \".*\"#dockerBase: \"$DOCKER_REPO\"#" $THIS_DIR/values.yaml
