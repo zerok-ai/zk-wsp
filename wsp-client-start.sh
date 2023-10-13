fca1766d0fc48050981f5e0cbe592bcf78ade006
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-10-13 14:18:39 +0530

    Made changes to start script

--- wsp-client-start.sh
+++ wsp-client-start.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-ARCH=$(uname -m)
-if [ "$ARCH" = "aarch64" ]; then
+arch=$(uname -m)
+
+if [ "$arch" = "x86_64" ] || [ "$arch" = "amd64" ]; then
+    echo "This is an AMD64 system."
+    ./myapp-amd64 -c /opt/wsp-config.yaml
+elif [ "$arch" = "aarch64" ] || [ "$arch" = "arm64" ]; then
+    echo "This is an ARM64 system."
     ./myapp-arm64 -c /opt/wsp-config.yaml
 else
-    ./myapp-amd64 -c /opt/wsp-config.yaml
+    echo "Unsupported Architecture: $arch"
 fi
